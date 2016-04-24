3b9f185d9787abe95f735d31ea107ce8eb544382
James Halliday <mail@substack.net>
2016-04-24 15:45:44 -0700

    functional client

---
+++ index.js
@@ -0,0 +1,76 @@
+var hyperlog = require('hyperlog')
+var keys = require('hyperreal/keys')
+var swarm = require('webrtc-swarm')
+var signalhub = require('signalhub')
+var inherits = require('inherits')
+var EventEmitter = require('events').EventEmitter
+var has = require('has')
+var wswarm = require('webrtc-swarm')
+var onend = require('end-of-stream')
+var pump = require('pump')
+var through = require('through2')
+var sub = require('subleveldown')
+var shasum = require('shasum')
+
+module.exports = Chat
+inherits(Chat, EventEmitter)
+
+function Chat (nym, db) {
+  if (!(this instanceof Chat)) return new Chat(nym, db)
+  EventEmitter.call(this)
+  this.db = db
+  this.logs = {}
+  this.swarms = {}
+  this.peers = {}
+  this.onswarm = {}
+  this.nym = nym
+  this.hubs = [ 'https://signalhub.mafintosh.com/' ]
+}
+
+Chat.prototype.join = function (channel) {
+  var self = this
+  if (has(self.swarms, channel)) return
+  self.logs[channel] = hyperlog(
+    sub(self.db, channel), { valueEncoding: 'json' })
+
+  self.logs[channel].createReadStream({ live: true })
+    .on('data', function (row) {
+      self.emit('say', channel, row)
+    })
+
+  self.emit('join', channel)
+  var hub = signalhub(shasum('chatwizard.' + channel), self.hubs)
+  var swarm = wswarm(hub)
+  self.swarms[channel] = swarm
+  self.peers[channel] = {}
+  console.log('onswarm')
+  self.onswarm[channel] = function (peer, id) {
+    console.log('PEER', id)
+    self.peers[channel][id] = peer
+    peer.pipe(self.logs[channel].replicate({ live: true })).pipe(peer)
+  }
+  swarm.on('peer', self.onswarm[channel])
+}
+
+Chat.prototype.part = function (channel) {
+  var self = this
+  if (!has(self.swarms, channel)) return
+  delete self.logs[channel]
+  self.swarms[channel].removeListener('peer', self.onswarm[channel])
+  delete self.swarms[channel]
+  delete self.onswarm[channel]
+  Object.keys(self.peers[channel]).forEach(function (key) {
+    self.peers[channel][key].destroy()
+  })
+  delete self.peers[channel]
+  self.emit('part', channel)
+}
+
+Chat.prototype.say = function (channel, msg) {
+  var data = {
+    time: Date.now(),
+    who: this.nym,
+    message: msg
+  }
+  this.logs[channel].append(data, function (err, node) {})
+}
--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 var strftime = require('strftime')
 var html = require('yo-yo')
 var uniq = require('uniq')
+var randomBytes = require('randombytes')
 
 var root = document.querySelector('#content')
 
 var state = {
   channels: [],
   channel: '(status)',
-  lines: []
+  nym: randomBytes(3).toString('hex'),
+  lines: {}
 }
 var memdb = require('memdb')
-var chat = require('./index.js')(memdb())
+var chat = require('./index.js')(state.nym, memdb())
 chat.on('join', function (channel) {
   state.channels.push(channel)
   uniq(state.channels)
@@ -22,6 +24,15 @@ chat.on('part', function (channel) {
   var ix = state.channels.indexOf(channel)
   if (ix >= 0) state.channels.splice(ix, 1)
   state.channel = state.channels[Math.max(0,ix-1)] || '(status)'
+  update()
+})
+
+chat.on('say', function (channel, row) {
+  if (!state.lines[channel]) state.lines[channel] = []
+  state.lines[channel].push(row)
+  state.lines[channel].sort(function (a, b) {
+    return a.value.time < b.value.time ? -1 : 1
+  })
   update()
 })
 
@@ -47,7 +58,8 @@ function render (state) {
       })}
     </div></div>
     <div class="lines"><div class="inner">
-      ${state.lines.map(function (m) {
+      ${(state.lines[state.channel] || []).map(function (row) {
+        var m = row.value
         return html`<div class="line">
           <span class="time">${strftime('%T', new Date(m.time))}</span>
           <span class="who">${'<' + m.who + '>'}</span>
@@ -66,19 +78,23 @@ function render (state) {
     ev.preventDefault()
     var msg = this.elements.text.value
     handleMsg(msg)
+    this.elements.text.value = ''
   }
 }
 
 function handleMsg (msg) {
   var m = /^\/(\S+)/.exec(msg)
   var cmd = (m && m[1] || '').toLowerCase()
-  if (cmd === 'join') {
+  if (cmd === 'join' || cmd === 'j') {
     chat.join(msg.split(/\s+/)[1] || state.channel)
-  } else if (cmd === 'part') {
+  } else if (cmd === 'part' || cmd === 'p') {
     chat.part(msg.split(/\s+/)[1] || state.channel)
+  } else if (cmd === 'nick' || cmd === 'n') {
+    chat.nym = msg.split(/\s+/)[1]
+    update()
   } else if (cmd) {
     // unknown command
-  } else {
+  } else if (state.channel !== '(status)') {
     chat.say(state.channel, msg)
   }
 }
